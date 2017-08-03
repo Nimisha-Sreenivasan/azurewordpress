f4b0c0b6b011efa65d4dce00c5f1dad1c68282ba
Nimisha Sreenivasan <nimisha.sreenivasan@sysfore.in>
2017-08-03 14:27:12 +0530

    Update footer.php

--- wp-content/themes/twentyseventeen/footer.php
+++ wp-content/themes/twentyseventeen/footer.php
@@ -15,7 +15,7 @@
 ?>
 
 		</div><!-- #content -->
-        <h4>Final test</h4>
+        <h4>Final test 03/08/2017</h4>
 		<footer id="colophon" class="site-footer" role="contentinfo" style="font-weight: 600;">
 			<div class="wrap">
 				<?php
